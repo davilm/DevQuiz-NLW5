7b2b4f112575bf56c91fd9e662955b0d2b7bd36d
Davi Leal <lealm.davi@gmail.com>
2021-04-19 23:59:29 -0300

    add ]splash page

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,3 +1,4 @@
+import 'package:DevQuiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
@@ -5,28 +6,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("NLW5 Flutter"),
-        ),
-        body: Center(
-          child: Container(
-            decoration: BoxDecoration(
-                color: Colors.purple, borderRadius: BorderRadius.circular(10)),
-            child: Text(
-              "Olá mundo",
-              style: TextStyle(
-                fontSize: 30,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.add),
-        ),
-      ),
+      home: SplashPage(),
     );
   }
 }
